2302452b802c14861a93a6378781a8c8b405a47b
Fábio Lima <limavfabio@gmail.com>
2023-10-08 11:28:34 -0300

    Fixes syntax on User model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable,
+         :confirmable
 
   # Associations
   has_many :job_listings, dependent: :destroy
