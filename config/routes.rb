9496894acff2af35e038965a3e9dbcda76b90ae4
Fábio Lima <limavfabio@gmail.com>
2023-10-09 19:26:15 -0300

    Clean up router

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :job_listings
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "job_listings#index"
 end
